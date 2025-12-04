6f30fa78b444b6c7f1d6d9e90b9fff49d13184b2
anrunt <grunt.janek@gmail.com>
2025-12-04 18:33:32 +0100

    Initial config build logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,56 @@
-use std::error::Error;
+use std::{error::Error, io::{self, Write}};
+pub struct Config {
+    path: String,
+    start_range: i32,
+    end_range: i32
+}
+
+impl Config {
+    pub fn build() -> Result<Config, Box<dyn Error>> {
+        let mut input: String = String::new();
+
+        print!("Give start and end range separated by space: ");
+        io::stdout().flush()?;
+        io::stdin().read_line(&mut input)?;
+
+        if input.trim().is_empty() {
+            return Err("No ranges provided - expected 2".into());
+        }
+
+        let mut iter = input.split_whitespace();
+
+        // Check if start_range is not bigger than end range
+        let start_range: i32 = iter.next()
+            .ok_or("Missing range argument (should be 2 only 1 provided)")?
+            .parse()
+            .map_err(|_| "Erorr with start_range")?;
+
+        let end_range: i32 = iter.next()
+            .ok_or("Missing range argument (should be 2 only 1 provided)")?
+            .parse()
+            .map_err(|_| "Error with end_range")?;
+
+        if iter.next().is_some() {
+            return Err("Too many arguments - expected exactly 2".into())
+        };
+
+        if start_range > end_range {
+            return Err("Start range can't be bigger than end range".into());
+        }
+
+        input.clear();
+
+        print!("Give pdf filename: ");
+        io::stdout().flush()?;
+        io::stdin().read_line(&mut input)?;
+
+        let path = parse_and_validate_filename(&input)?;
+
+        println!("Start_range: {}, end_range: {}, path: {}", start_range, end_range, path);
+
+        Ok(Config {start_range, end_range, path})
+    }
+}
 
 pub fn parse_and_validate_filename(input: &str) -> Result<String, Box<dyn Error>> {
     let path_trimmed_string= input.trim().to_string();
@@ -16,6 +68,10 @@ pub fn parse_and_validate_filename(input: &str) -> Result<String, Box<dyn Error>
 
     if dot_count > 1 {
         return Err("Filename contains more than one dot!".into());
+    }
+
+    if !path.ends_with(".pdf") {
+        return Err("File must be of pdf type".into());
     }
 
     Ok(path)
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use std::{error::Error, io::{self, Write}};
-
-use pdf_splitter::parse_and_validate_filename;
+use pdf_splitter::{Config};
 
 fn main() {
     let config = Config::build();
@@ -10,45 +8,5 @@ fn main() {
             println!("Error with building config: {e}");
             std::process::exit(1);
         }
-    }
-}
-
-struct Config {
-    path: String,
-    start_range: i32,
-    end_range: i32
-}
-
-impl Config {
-    fn build() -> Result<Config, Box<dyn Error>> {
-        let mut input: String = String::new();
-
-        print!("Give start and end range separated by space: ");
-        io::stdout().flush()?;
-        io::stdin().read_line(&mut input)?;
-
-        let mut iter = input.split_whitespace();
-
-        let start_range: i32 = iter.next()
-            .ok_or("Missing range argument (should be 2 only 1 provided)")?
-            .parse()
-            .map_err(|_| "Erorr with start_range")?;
-
-        let end_range: i32 = iter.next()
-            .ok_or("Missing range argument (should be 2 only 1 provided)")?
-            .parse()
-            .map_err(|_| "Error with end_range")?;
-
-        input.clear();
-
-        print!("Give pdf filename: ");
-        io::stdout().flush()?;
-        io::stdin().read_line(&mut input)?;
-
-        let path = parse_and_validate_filename(&input)?;
-
-        println!("Start_range: {}, end_range: {}, path: {}", start_range, end_range, path);
-
-        Ok(Config {start_range, end_range, path})
     }
 }
